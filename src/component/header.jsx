2fd9ae63f1a371e515960cbdbe8d0fb8c3a7fcfe
unknown <dianyusufmuldani@gmail.com>
2023-02-16 10:35:40 +0700

    add cy header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -4,8 +4,10 @@ import ImageHeader from "../assets/header_background.png";
 const HeaderApp = () => {
   return (
     <div className="header" data-cy="header">
-      {/* <img src={ImageHeader} alt="ImageHeader" width={"100%"} height="105px" /> */}
-      <h2 className="titleApp">To Do List App</h2>
+      <span className="titleApp" data-cy="header-title">
+        To Do List App
+      </span>
+      {/* <h2 ></h2> */}
     </div>
   );
 };
