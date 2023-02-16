5d04a96c5f51299eb88dda841a6f4f057da5d591
unknown <dianyusufmuldani@gmail.com>
2023-02-16 23:02:32 +0700

    add title

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -5,6 +5,13 @@ const Dashboard = () => {
   return (
     <>
       <HeaderApp />
+      <main className="mx-auto flex w-full max-w-[1000px] flex-col py-11">
+        <div className="mb-11 flex">
+          <h1 data-cy="activity-title" className="text-4xl font-bold">
+            Activity
+          </h1>
+        </div>
+      </main>
     </>
   );
 };
