5e957ec008ee8aae028896794459eddc9d5ce6b1
unknown <dianyusufmuldani@gmail.com>
2023-02-17 15:17:14 +0700

    fix delete activity

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -9,6 +9,7 @@ const Dashboard = () => {
   const [showModal, setShowModal] = React.useState(false);
   const [itemForDelete, setItemForDelete] = useState(null);
   const [showModalToasted, setModalToasted] = useState(false);
+  console.log("item ini", itemForDelete);
   useEffect(() => {
     axios
       .get(
@@ -85,12 +86,12 @@ const Dashboard = () => {
         <div className="grid grid-cols-4 gap-5">
           {data.map((item, index) => {
             return (
-              <>
+              <div>
                 <a
                   href={`/detail/${item.id}`}
                   key={index}
                   data-cy="activity-item"
-                  className="flex h-60 flex-col rounded-xl bg-white p-6 text-left shadow-lg"
+                  className="flex h-60 flex-col rounded-xl bg-white p-6 text-left shadow-lg "
                 >
                   <h3
                     data-cy="activity-item-title"
@@ -105,29 +106,29 @@ const Dashboard = () => {
                     >
                       {moment(item.created_at).format("LL")}
                     </span>
-                    <button
-                      onClick={() => handleModalDelete(item)}
-                      data-cy="activity-item-delete-button"
-                      className="ml-auto"
-                    >
-                      <svg
-                        width={24}
-                        height={24}
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"
-                          stroke="#888"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>
-                      </svg>
-                    </button>
                   </div>
                 </a>
-              </>
+                <button
+                  onClick={() => handleModalDelete(item)}
+                  data-cy="activity-item-delete-button"
+                  className="ml-auto"
+                >
+                  <svg
+                    width={24}
+                    height={24}
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"
+                      stroke="#888"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
             );
           })}
         </div>
@@ -163,7 +164,7 @@ const Dashboard = () => {
                 >
                   Apakah anda yakin menghapus activity
                   <br />
-                  <b>"New Activity"?</b>
+                  <b>{itemForDelete.title}?</b>
                 </p>
                 <div className="flex gap-5">
                   <button
