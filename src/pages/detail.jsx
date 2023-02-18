c017938fdb442c8b46daf22de3d87978fd9db15b
unknown <dianyusufmuldani@gmail.com>
2023-02-18 09:16:02 +0700

    empty item

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -27,7 +27,7 @@ const Detail = () => {
   const [lifecycle, setLifecycle] = useState(null);
   const params = useParams();
   const { id } = params;
-  console.log("isi data", dataOne);
+  console.log("isi data", data);
 
   useEffect(() => {
     if (data !== null || data !== undefined) {
@@ -55,7 +55,8 @@ const Detail = () => {
         request
       )
       .then((res) => {
-        console.log("Edited");
+        console.log("Edited", res);
+        setLifecycle(res.status);
       });
   };
 
@@ -225,106 +226,110 @@ const Detail = () => {
             Tambah
           </button>
         </div>
-        {/* <button data-cy="todo-empty-state" className="mx-auto mt-24">
-          <img src={ImageEmpty}></img>
-        </button> */}
-
-        {data.map((item, index) => {
-          return (
-            <div key={index} className="mt-5 flex flex-col gap-[10px]">
-              <div
-                data-cy="todo-item"
-                className="flex h-20 items-center gap-4 rounded-xl bg-white px-7 text-lg font-medium shadow-lg"
-              >
-                {item.is_active == 1 ? (
-                  <>
-                    <span
-                      onClick={() => handleActiveItems(item)}
-                      data-cy="todo-item-checkbox"
-                      className="border-secondary h-5 w-5 cursor-pointer border flex items-center justify-center z-20"
-                    ></span>
-                    <span
-                      data-cy="todo-item-priority-indicator"
-                      className="rounded-full h-[9px] w-[9px]"
-                    ></span>
-                    <h3 data-cy="todo-item-title">{item.title}</h3>
-                  </>
-                ) : (
-                  <>
-                    <span
-                      onClick={() => handleInActiveItems(item)}
-                      data-cy="todo-item-checkbox"
-                      className="h-5 w-5 cursor-pointer border flex items-center justify-center border-blue bg-primary  z-20"
+        {data.length === 0 ? (
+          <button data-cy="todo-empty-state" className="mx-auto mt-24">
+            <img src={ImageEmpty}></img>
+          </button>
+        ) : (
+          <>
+            {data.map((item, index) => {
+              return (
+                <div key={index} className="mt-5 flex flex-col gap-[10px]">
+                  <div
+                    data-cy="todo-item"
+                    className="flex h-20 items-center gap-4 rounded-xl bg-white px-7 text-lg font-medium shadow-lg"
+                  >
+                    {item.is_active == 1 ? (
+                      <>
+                        <span
+                          onClick={() => handleActiveItems(item)}
+                          data-cy="todo-item-checkbox"
+                          className="border-secondary h-5 w-5 cursor-pointer border flex items-center justify-center z-20"
+                        ></span>
+                        <span
+                          data-cy="todo-item-priority-indicator"
+                          className="rounded-full h-[9px] w-[9px]"
+                        ></span>
+                        <h3 data-cy="todo-item-title">{item.title}</h3>
+                      </>
+                    ) : (
+                      <>
+                        <span
+                          onClick={() => handleInActiveItems(item)}
+                          data-cy="todo-item-checkbox"
+                          className="h-5 w-5 cursor-pointer border flex items-center justify-center border-blue bg-primary  z-20"
+                        >
+                          <svg
+                            width={14}
+                            height={14}
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="m2.917 7 2.916 2.917 5.833-5.834"
+                              stroke="#fff"
+                              strokeWidth={2}
+                              strokeLinecap="square"
+                            ></path>
+                          </svg>
+                        </span>
+                        <span
+                          data-cy="todo-item-priority-indicator"
+                          className="rounded-full h-[9px] w-[9px]"
+                        ></span>
+                        <h3 data-cy="todo-item-title">
+                          <s>{item.title}</s>
+                        </h3>
+                      </>
+                    )}
+                    <button
+                      data-cy="todo-item-edit-button"
+                      onClick={() => setShowModalEditItem(true)}
                     >
                       <svg
-                        width={14}
-                        height={14}
+                        width={24}
+                        height={24}
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          d="m2.917 7 2.916 2.917 5.833-5.834"
-                          stroke="#fff"
+                          d="M4 20h4L18.5 9.5a2.829 2.829 0 0 0-4-4L4 16v4ZM13.5 6.5l4 4"
+                          stroke="#A4A4A4"
                           strokeWidth={2}
-                          strokeLinecap="square"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         ></path>
                       </svg>
-                    </span>
-                    <span
-                      data-cy="todo-item-priority-indicator"
-                      className="rounded-full h-[9px] w-[9px]"
-                    ></span>
-                    <h3 data-cy="todo-item-title">
-                      <s>{item.title}</s>
-                    </h3>
-                  </>
-                )}
-                <button
-                  data-cy="todo-item-edit-button"
-                  onClick={() => setShowModalEditItem(true)}
-                >
-                  <svg
-                    width={24}
-                    height={24}
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 20h4L18.5 9.5a2.829 2.829 0 0 0-4-4L4 16v4ZM13.5 6.5l4 4"
-                      stroke="#A4A4A4"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                  </svg>
-                </button>
-                <button
-                  data-cy="todo-item-delete-button"
-                  className="ml-auto z-20"
-                  onClick={() => {
-                    setShowModalDelete(true);
-                    setDataOne(item);
-                  }}
-                >
-                  <svg
-                    width={24}
-                    height={24}
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"
-                      stroke="#888"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          );
-        })}
+                    </button>
+                    <button
+                      data-cy="todo-item-delete-button"
+                      className="ml-auto z-20"
+                      onClick={() => {
+                        setShowModalDelete(true);
+                        setDataOne(item);
+                      }}
+                    >
+                      <svg
+                        width={24}
+                        height={24}
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"
+                          stroke="#888"
+                          strokeWidth={2}
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        )}
 
         <ModalAddItems
           handleAddItems={handleAddItems}
